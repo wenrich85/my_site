a94c6196ddd64679ad43c1176293bf51bba075cd
wenrich85 <wenrich85@gmail.com>
2017-09-23 15:04:17 -0400

    added comment module

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
 	def get_queryset(self):
@@ -25,6 +26,7 @@ class Post(models.Model):
 	objects =models.Manager() # The deg=fault manager.
 	published = PublishedManager() # The Dahl-specific manager.
 	
+	tags = TaggableManager()
 	class Meta:
 		ordering = ('-publish',)
 
@@ -37,3 +39,24 @@ class Post(models.Model):
 												 self.publish.strftime('%d'),
 												 self.slug
 												])
+
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, related_name='comments')
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created =models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+
+	class Meta:
+		ordering=('created',)
+
+	
+	def __str__(self):
+		return 'Comment by {} on {}'.format(self.name, self.post)
+
+
+
+	
