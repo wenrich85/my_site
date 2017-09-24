20ed8767f67995bff93b67c255e5fb71e6eb0ccf
wenrich85 <wenrich85@gmail.com>
2017-09-23 21:10:57 -0400

    added similar posts to details view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
-
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -57,13 +57,18 @@ def post_detail(request, year, month, day, post):
             #assign the current post to the comment
             new_comment.post = post
             #save the comment to the database
-            new_coment.save()
+            new_comment.save()
     else:
         comment_form = CommentForm()
 
+        post_tag_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form':comment_form})
+                                                     'comment_form':comment_form,
+                                                     'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     # Retrieve post by id
